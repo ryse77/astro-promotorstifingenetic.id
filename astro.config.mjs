61612ec6a507fe47d937c57f8a4bc78cabfbbe54
stifinmuslimah <roy.yulis77@gmail.com>
2025-12-16 21:05:07 +0700

    update CAPI

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://promotorstifingenetic.id',
+	adapter: cloudflare(),
 	integrations: [
 		tailwind(),
 		sitemap({
