f49ed399fce3c833dbc31b8b244bd8f60036290e
Roy Yulis Setiandi <roy.yulis77@gmail.com>
2026-02-12 16:18:35 +0700

    update social proof

--- src/lib/data/social-proof.ts
+++ src/lib/data/social-proof.ts
@@ -6,112 +6,254 @@ export interface SocialProofEntry {
 }
 
 export const socialProofData: SocialProofEntry[] = [
-  // ========================================
-  // CARA EDIT:
-  // 1. Copy block dibawah
-  // 2. Ganti name, city, dan avatar
-  // 3. Jika ada foto: avatar: "/images/avatars/nama.jpg"
-  // 4. Jika pakai initial: avatar: null
-  // ========================================
-
   {
-    name: "Ari Wibowo",
-    city: "Jakarta Selatan",
-    avatar: "/images/avatars/ari.png", // Custom photo
-  },
-  {
-    name: "Wisnu Pratama",
-    city: "Surabaya",
-    avatar: "/images/avatars/wisnu.png", // Custom photo
-  },
-  {
-    name: "Citra Dewi",
-    city: "Bandung",
+    name: "Jifi Abdullah",
+    city: "PADANG PARIAMAN",
     avatar: null,
   },
   {
-    name: "Dewi Lestari",
-    city: "Medan",
+    name: "Desmanita.A Md",
+    city: "KAB. AGAM",
     avatar: null,
   },
   {
-    name: "Eko Prasetyo",
-    city: "Semarang",
+    name: "Fitri Ningsih S. Psi",
+    city: "KAB. KAMPAR",
     avatar: null,
   },
   {
-    name: "Fitri Handayani",
-    city: "Makassar",
+    name: "Dwi Darmawati",
+    city: "TANGERANG",
     avatar: null,
   },
   {
-    name: "Gani Wijaya",
-    city: "Palembang",
+    name: "John Lauis",
+    city: "TANGERANG",
     avatar: null,
   },
   {
-    name: "Hana Pratiwi",
-    city: "Tangerang",
+    name: "Devinawati",
+    city: "TANGERANG",
     avatar: null,
   },
   {
-    name: "Indra Gunawan",
-    city: "Depok",
+    name: "Ahmad Suratman",
+    city: "TANGERANG",
     avatar: null,
   },
   {
-    name: "Joko Susilo",
-    city: "Bekasi",
+    name: "Wiwik Sriwahyuni",
+    city: "TANGERANG",
     avatar: null,
   },
   {
-    name: "Kartika Sari",
-    city: "Yogyakarta",
+    name: "Rona Agustina",
+    city: "TANGERANG",
     avatar: null,
   },
   {
-    name: "Laila Maharani",
-    city: "Malang",
+    name: "CHICHI SUKARDJO",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Maya Putri",
-    city: "Bogor",
+    name: "Firdiani",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Nina Rahayu",
-    city: "Batam",
+    name: "Marwi Argasasmita",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Oscar Ramadhan",
-    city: "Pekanbaru",
+    name: "Sylvia Martshalina",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Putri Ayu",
-    city: "Denpasar",
+    name: "SITI AMINAH",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Qori Hidayat",
-    city: "Balikpapan",
+    name: "Irfan Abdul Hilmi",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Rina Marlina",
-    city: "Pontianak",
+    name: "Tiantri Wulan Yuli",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Sari Wulandari",
-    city: "Samarinda",
+    name: "Budi Rinoso",
+    city: "JAKARTA TIMUR",
     avatar: null,
   },
   {
-    name: "Tono Kusuma",
-    city: "Manado",
+    name: "Dinda Riah Sari",
+    city: "JAKARTA TIMUR",
+    avatar: null,
+  },
+  {
+    name: "CITRA SETYO PRATIWI",
+    city: "JAKARTA TIMUR",
+    avatar: null,
+  },
+  {
+    name: "Suharti SE A.Md",
+    city: "JAKARTA TIMUR",
+    avatar: null,
+  },
+  {
+    name: "dr Alief Ringga Prapditia Sugiyo",
+    city: "JAKARTA TIMUR",
+    avatar: null,
+  },
+  {
+    name: "Yuliningsih",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "YURIKE OKTIVIA ISKANDAR",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "Yenny Shofariah",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "Fazha Farhinna S.Psi",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "Ratu Nabila Kurnia Rahman",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "Abdul Halim Nasution",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "A.LARASATI SUGITY",
+    city: "JAKARTA SELATAN",
+    avatar: null,
+  },
+  {
+    name: "Reza Muhammad Syarief",
+    city: "DEPOK",
+    avatar: null,
+  },
+  {
+    name: "Rahmawati Dimyati",
+    city: "DEPOK",
+    avatar: null,
+  },
+  {
+    name: "dr. Fitria Yunita",
+    city: "DEPOK",
+    avatar: null,
+  },
+  {
+    name: "Bayu Yuda Pribadi",
+    city: "DEPOK",
+    avatar: null,
+  },
+  {
+    name: "Renanta Anindia Wardana S.S.T.Pi. M.M.",
+    city: "DEPOK",
+    avatar: null,
+  },
+  {
+    name: "Septy Maharani Rinal",
+    city: "BANDA ACEH",
+    avatar: null,
+  },
+  {
+    name: "Devisa Pertiwi",
+    city: "BANDA ACEH",
+    avatar: null,
+  },
+  {
+    name: "Eny Sriwahyuningsih",
+    city: "SEMARANG",
+    avatar: null,
+  },
+  {
+    name: "Florencia Angela Santoso",
+    city: "SEMARANG",
+    avatar: null,
+  },
+  {
+    name: "Brillianthine Novy Apsari",
+    city: "SEMARANG",
+    avatar: null,
+  },
+  {
+    name: "Ivan Kurnia Adi",
+    city: "SEMARANG",
+    avatar: null,
+  },
+  {
+    name: "Adelina Cahyaningrum S.Tr.Keb",
+    city: "SEMARANG",
+    avatar: null,
+  },
+  {
+    name: "Ahda Syakira",
+    city: "Padang",
+    avatar: null,
+  },
+  {
+    name: "Nuraisyah Lubis",
+    city: "BATAM",
+    avatar: null,
+  },
+  {
+    name: "Annisa Febrinel Hendry",
+    city: "BOGOR",
+    avatar: null,
+  },
+  {
+    name: "Agus Siswanto",
+    city: "BOGOR",
+    avatar: null,
+  },
+  {
+    name: "ERVAN ZUMAEDI",
+    city: "BOGOR",
+    avatar: null,
+  },
+  {
+    name: "Yusuf",
+    city: "BOGOR",
+    avatar: null,
+  },
+  {
+    name: "Muhammad Isa Afifi S.T",
+    city: "BOGOR",
+    avatar: null,
+  },
+  {
+    name: "Merry Ajeng Putri Dewi",
+    city: "BANDUNG BARAT",
+    avatar: null,
+  },
+  {
+    name: "Ismail Saleh",
+    city: "BANDUNG BARAT",
+    avatar: null,
+  },
+  {
+    name: "Tommy Nugraha",
+    city: "BANDUNG BARAT",
     avatar: null,
   },
 ];
@@ -120,7 +262,6 @@ export const messageTemplates = [
   "{name} dari {city} baru saja mendaftar sebagai Licensed Promotor STIFIn",
   "{name} dari {city} telah bergabung menjadi Promotor STIFIn",
   "{name} di {city} baru mendaftar",
-  "Seseorang dari {city} baru saja mengambil peluang ini",
 ];
 
 // Helper function untuk generate random entry
